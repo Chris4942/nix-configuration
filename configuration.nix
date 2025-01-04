2f8fc08ea7b5ea51364ebf9c08b0a8be985b75f4
Chris West <cjwest4942@gmail.com>
2025-01-04 11:54:49 -0500

    switch to using a flake configuration

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      /etc/nixos/hardware-configuration.nix
+      ./hardware/amd-gpu.nix
     ];
 
   nix.settings.experimental-features = [ "nix-command"  "flakes" ];
