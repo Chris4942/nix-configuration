53f918915545ecb93229dee746e17bd5f00c9a68
Chris West <cjwest4942@gmail.com>
2025-02-15 13:21:22 -0500

    added comment about how to fix gnome mouse missing

--- modules/desktop-environment/gnome.nix
+++ modules/desktop-environment/gnome.nix
@@ -3,4 +3,6 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  # you may need to run the following command if switching from plasma. There might be a way to bake this into the nix config so that it's not required manually.
+  # dconf reset /org/gnome/desktop/interface/cursor-theme
 }
--- modules/nvf/keymaps/base.nix
+++ modules/nvf/keymaps/base.nix
@@ -1,114 +1,114 @@
 [
-          {
-            key = "<leader>r";
-            mode = "n";
-            silent = true;
-            action = ":term<CR>";
-            desc = "open terminal";
-          }
-          {
-            key = "<leader>bb";
-            mode = "n";
-            silent = true;
-            action = ":b#<CR>";
-            desc = "open previous buffer";
-          }
-          {
-            key = "<C-h>";
-            mode = "n";
-            silent = true;
-            action = "<C-w>h";
-            desc = "move to left window";
-          }
-          {
-            key = "<C-j>";
-            mode = "n";
-            silent = true;
-            action = "<C-w>j";
-            desc = "move to left window";
-          }
-          {
-            key = "<C-k>";
-            mode = "n";
-            silent = true;
-            action = "<C-w>k";
-            desc = "move to left window";
-          }
-          {
-            key = "<C-l>";
-            mode = "n";
-            silent = true;
-            action = "<C-w>l";
-            desc = "move to left window";
-          }
-          {
-            key = "<leader>fy";
-            mode = "n";
-            silent = true;
-            action = ":let @+ = expand('%:.')<cr>";
-            desc = "copy relative path to clipboard";
-          }
-          {
-            key = "<leader>fY";
-            mode = "n";
-            silent = true;
-            action = ":let @+ = expand('%:p')<cr>";
-            desc = "copy absolute path to clipboard";
-          }
-          {
-            key = "<leader>fj";
-            mode = "n";
-            silent = true;
-            action = ":let @+ = expand('%:t')<cr>";
-            desc = "Copy short file name to clipboard";
-          }
-          {
-            key = "<leader>fe";
-            mode = "n";
-            silent = true;
-            action = "<cmd>Neotree reveal<cr>";
-            desc = "Reveal current file in file tree";
-          }
-          {
-            key = "<leader>nh";
-            mode = "n";
-            silent = true;
-            action = "<cmd>noh<cr>";
-            desc = "Turn off search highlights (until next search)";
-          }
-          {
-            key = "<leader>bd";
-            mode = "n";
-            silent = true;
-            action = "<cmd>bp<bar>sp<bar>bn<bar>bd<cr>";
-            desc = "Close current buffer (TODO this macro is kind of broken)";
-          }
-          {
-            key = "<leader>bo";
-            mode = "n";
-            silent = true;
-            action = "<cmd>%bd<bar>e#<cr>";
-            desc = "Close all other bothers";
-          }
-          {
-            key = "<C-s>";
-            mode = [
-              "i"
-              "n"
-            ];
-            action = "<cmd>w<cr>";
-            desc = "save current buffer ( same as `:w`)";
-          }
-          {
-            key = "<leader>fi";
-            mode = "n";
-            action = ":let @+ = expand('%:.') . ':' . line('.')<cr>";
-            desc = "copy relative file path with line number";
-          }
-          {
-            key = "<leader>fI";
-            mode = "n";
-            action = ":let @+ = expand('%:p') . ':' . line('.')<cr>";
-            desc = "copy absolute file path with line number";
-          }
-        ]
+  {
+    key = "<leader>r";
+    mode = "n";
+    silent = true;
+    action = ":term<CR>";
+    desc = "open terminal";
+  }
+  {
+    key = "<leader>bb";
+    mode = "n";
+    silent = true;
+    action = ":b#<CR>";
+    desc = "open previous buffer";
+  }
+  {
+    key = "<C-h>";
+    mode = "n";
+    silent = true;
+    action = "<C-w>h";
+    desc = "move to left window";
+  }
+  {
+    key = "<C-j>";
+    mode = "n";
+    silent = true;
+    action = "<C-w>j";
+    desc = "move to left window";
+  }
+  {
+    key = "<C-k>";
+    mode = "n";
+    silent = true;
+    action = "<C-w>k";
+    desc = "move to left window";
+  }
+  {
+    key = "<C-l>";
+    mode = "n";
+    silent = true;
+    action = "<C-w>l";
+    desc = "move to left window";
+  }
+  {
+    key = "<leader>fy";
+    mode = "n";
+    silent = true;
+    action = ":let @+ = expand('%:.')<cr>";
+    desc = "copy relative path to clipboard";
+  }
+  {
+    key = "<leader>fY";
+    mode = "n";
+    silent = true;
+    action = ":let @+ = expand('%:p')<cr>";
+    desc = "copy absolute path to clipboard";
+  }
+  {
+    key = "<leader>fj";
+    mode = "n";
+    silent = true;
+    action = ":let @+ = expand('%:t')<cr>";
+    desc = "Copy short file name to clipboard";
+  }
+  {
+    key = "<leader>fe";
+    mode = "n";
+    silent = true;
+    action = "<cmd>Neotree reveal<cr>";
+    desc = "Reveal current file in file tree";
+  }
+  {
+    key = "<leader>nh";
+    mode = "n";
+    silent = true;
+    action = "<cmd>noh<cr>";
+    desc = "Turn off search highlights (until next search)";
+  }
+  {
+    key = "<leader>bd";
+    mode = "n";
+    silent = true;
+    action = "<cmd>bp<bar>sp<bar>bn<bar>bd<cr>";
+    desc = "Close current buffer (TODO this macro is kind of broken)";
+  }
+  {
+    key = "<leader>bo";
+    mode = "n";
+    silent = true;
+    action = "<cmd>%bd<bar>e#<cr>";
+    desc = "Close all other bothers";
+  }
+  {
+    key = "<C-s>";
+    mode = [
+      "i"
+      "n"
+    ];
+    action = "<cmd>w<cr>";
+    desc = "save current buffer ( same as `:w`)";
+  }
+  {
+    key = "<leader>fi";
+    mode = "n";
+    action = ":let @+ = expand('%:.') . ':' . line('.')<cr>";
+    desc = "copy relative file path with line number";
+  }
+  {
+    key = "<leader>fI";
+    mode = "n";
+    action = ":let @+ = expand('%:p') . ':' . line('.')<cr>";
+    desc = "copy absolute file path with line number";
+  }
+]
