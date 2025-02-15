6159e7e13aa86a354e90792c56e24b5141bba516
Chris West <cjwest4942@gmail.com>
2025-02-15 13:58:30 -0500

    move cwest config into its own file

--- flake.nix
+++ flake.nix
@@ -18,43 +18,14 @@
       home-manager,
       nvf,
       ...
-    }:
+    }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       nixosConfigurations = {
-        cwest-nixos-1 = nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit home-manager nvf;
-            rootUser = rec {
-              name = "cwest";
-              description = "Chris West";
-              homeDirectory = "/home/${name}";
-            };
-          };
-          modules = [
-            ./hardware/machines/cwest-nixos-1/configuration.nix
-            ./modules/fonts.nix
-            ./modules/localization/new-york.nix
-            ./modules/monitor-configuration.nix
-            ./modules/bash.nix
-            ./modules/syncthing.nix
-            ./modules/nix-ld.nix
-            ./modules/xinput.nix
-            ./modules/steam.nix
-            ./modules/audio.nix
-            ./modules/nvf/module.nix
-            ./hardware/machines/cwest-nixos-1/hardware.nix
-            ./hardware/drivers/amdgpu.nix
-            ./modules/desktop-environment/gnome.nix
-            ./modules/desktop-environment/sway.nix
-            nvf.nixosModules.default
-            home-manager.nixosModules.default
-                                                ./modules/libre-office.nix
-          ];
-        };
+        cwest-nixos-1 = import ./hardware/machines/cwest-nixos-1 inputs;
 
         # Nathan, feel free to change this to whatever you need
         nadaph-nixos-1 = nixpkgs.lib.nixosSystem {
---
+++ hardware/machines/cwest-nixos-1/default.nix
@@ -0,0 +1,36 @@
+{
+  nixpkgs,
+  home-manager,
+  nvf,
+  ...
+}:
+nixpkgs.lib.nixosSystem {
+  specialArgs = {
+    inherit home-manager nvf;
+    rootUser = rec {
+      name = "cwest";
+      description = "Chris West";
+      homeDirectory = "/home/${name}";
+    };
+  };
+  modules = [
+    ./configuration.nix
+    ../../../modules/fonts.nix
+    ../../../modules/localization/new-york.nix
+    ../../../modules/monitor-configuration.nix
+    ../../../modules/bash.nix
+    ../../../modules/syncthing.nix
+    ../../../modules/nix-ld.nix
+    ../../../modules/xinput.nix
+    ../../../modules/steam.nix
+    ../../../modules/audio.nix
+    ../../../modules/nvf/module.nix
+    ../../../hardware/machines/cwest-nixos-1/hardware.nix
+    ../../../hardware/drivers/amdgpu.nix
+    ../../../modules/desktop-environment/gnome.nix
+    ../../../modules/desktop-environment/sway.nix
+    ../../../modules/libre-office.nix
+    nvf.nixosModules.default
+    home-manager.nixosModules.default
+  ];
+}
