fffda672ed1f4a7e4e87a1501a5d93b9061b673a
Chris West <cjwest4942@gmail.com>
2025-02-27 23:01:16 -0500

    add a comment with the garbage collection command

--- modules/automated-garbage-collection.nix
+++ modules/automated-garbage-collection.nix
@@ -1,5 +1,7 @@
 { ... }:
 {
+  # this can be done manually instead with
+  # nix-collect-garbage -d --delete-older-than 30d
   nix.gc.automatic = true;
   nix.gc.dates = "daily";
   nix.gc.options = "--delete-older-than 30d";
