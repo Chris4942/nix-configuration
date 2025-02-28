8c8b8bb4059e0837d7859eb85de35164dc875ec1
Chris West <cjwest4942@gmail.com>
2025-02-27 21:05:11 -0500

    uncomment all the other modules

--- hardware/machines/cwest-usb/default.nix
+++ hardware/machines/cwest-usb/default.nix
@@ -36,16 +36,16 @@ nixpkgs.lib.nixosSystem {
     ./configuration.nix
     ./hardware.nix
     ../../../modules/fonts.nix
-    # ../../../modules/localization/new-york.nix
-    # ../../../modules/bash.nix
-    # ../../../modules/nix-ld.nix
-    #             # ../../../modules/xinput.nix
-    #             # ../../../modules/steam.nix
-    # ../../../modules/audio.nix
-    # # ../../../modules/nvf/module.nix
-    # ../../../modules/desktop-environment/gnome.nix
-    # ../../../modules/desktop-environment/sway.nix
-    # ../../../modules/libre-office.nix
+    ../../../modules/localization/new-york.nix
+    ../../../modules/bash.nix
+    ../../../modules/nix-ld.nix
+    ../../../modules/xinput.nix
+    ../../../modules/steam.nix
+    ../../../modules/audio.nix
+    ../../../modules/nvf/module.nix
+    ../../../modules/desktop-environment/gnome.nix
+    ../../../modules/desktop-environment/sway.nix
+    ../../../modules/libre-office.nix
     nvf.nixosModules.default
     home-manager.nixosModules.default
   ];
