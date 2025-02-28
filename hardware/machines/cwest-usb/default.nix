806851b72a116717d637a77bf8280d1a32ef3a26
Chris West <cjwest4942@gmail.com>
2025-02-27 19:42:22 -0500

    nix fmt

--- hardware/machines/cwest-usb/default.nix
+++ hardware/machines/cwest-usb/default.nix
@@ -1,13 +1,14 @@
-{ nixpkgs, ... }: nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            (
-              { pkgs, modulesPath, ... }:
-              {
-                imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
-                environment.systemPackages = [ pkgs.neovim ];
-              }
-            )
-                # ../cwest-nixos-1/configuration.nix
-          ];
-        }
+{ nixpkgs, ... }:
+nixpkgs.lib.nixosSystem {
+  system = "x86_64-linux";
+  modules = [
+    (
+      { pkgs, modulesPath, ... }:
+      {
+        imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
+        environment.systemPackages = [ pkgs.neovim ];
+      }
+    )
+    # ../cwest-nixos-1/configuration.nix
+  ];
+}
