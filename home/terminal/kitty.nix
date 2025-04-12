22cac6d8c129c59b7cc25d235bb592094ee71e7c
Chris West <64553509+Chris4942@users.noreply.github.com>
2025-04-12 18:57:59 -0400

    Switch to using the kitty terminal cause it's better (#11)
    * add kitty to system packages
    
    * move sway home configruation into its own directory
    
    * kitty is working!
    
    * switch to use the kitty terminal in sway

---
+++ home/terminal/kitty.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  pkgs,
+  home-manager,
+  lib,
+  rootUser,
+  ...
+}:
+{
+  programs.kitty = {
+    enable = true;
+    # kitty has catppuccin theme built-in,
+    # all the built-in themes are packaged into an extra package named `kitty-themes`
+    # and it's installed by home-manager if `theme` is specified.
+    themeFile = "gruvbox-dark";
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      # use different font size on macOS
+      size = if pkgs.stdenv.isDarwin then 14 else 13;
+    };
+
+    # consistent with other terminal emulators
+    keybindings = {
+      "ctrl+shift+f" = "show_scrollback"; # search in the current window
+    };
+
+    settings = {
+      background_opacity = "0.93";
+      macos_option_as_alt = true; # Option key acts as Alt on macOS
+      enable_audio_bell = false;
+      tab_bar_edge = "top"; # tab bar on top
+    };
+
+    # macOS specific settings
+    darwinLaunchOptions = [ "--start-as=maximized" ];
+  };
+}
