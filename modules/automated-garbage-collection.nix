022d95e63e6c70ae706d894cf88bbb691063460a
Chris West <cjwest4942@gmail.com>
2025-04-12 15:47:27 -0400

    update garbage collection

--- modules/automated-garbage-collection.nix
+++ modules/automated-garbage-collection.nix
@@ -3,7 +3,7 @@
   # this can be done manually instead with
   # nix-collect-garbage -d --delete-older-than 30d
   nix.gc.automatic = true;
-  nix.gc.dates = "daily";
+  nix.gc.dates = "monthly";
   nix.gc.options = "--delete-older-than 30d";
   nix.settings.auto-optimise-store = true;
 }
