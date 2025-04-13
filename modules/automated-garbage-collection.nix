fc95319ae2b4a55631cfee04df39dfc0ebbb981f
Chris West <cjwest4942@gmail.com>
2025-04-12 22:14:51 -0400

    delete generations older than 21 days

--- modules/automated-garbage-collection.nix
+++ modules/automated-garbage-collection.nix
@@ -1,9 +1,9 @@
 { ... }:
 {
   # this can be done manually instead with
-  # nix-collect-garbage -d --delete-older-than 30d
+  # nix-collect-garbage -d --delete-older-than 21d
   nix.gc.automatic = true;
   nix.gc.dates = "monthly";
-  nix.gc.options = "--delete-older-than 30d";
+  nix.gc.options = "--delete-older-than 21d";
   nix.settings.auto-optimise-store = true;
 }
