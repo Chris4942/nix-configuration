782510ac213727ecd1d4b805c004b6ee4c0dd4c8
Chris West <cjwest4942@gmail.com>
2025-04-14 22:46:46 -0400

    set the active border color

--- home/desktop-environments/hyprland.nix
+++ home/desktop-environments/hyprland.nix
@@ -68,6 +68,25 @@
             9
           )
         );
+      # https://wiki.hyprland.org/Configuring/Variables/#general
+      general = {
+        gaps_in = 5;
+        gaps_out = 20;
+
+        border_size = 2;
+
+        # https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
+        "col.active_border" = "rgba(ffa011df)";
+        "col.inactive_border" = "rgba(595959aa)";
+
+        # Set to true enable resizing windows by clicking and dragging on borders and gaps
+        resize_on_border = false;
+
+        # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
+        allow_tearing = false;
+
+        layout = "dwindle";
+      };
     };
     plugins = with pkgs.hyprlandPlugins; [
       hyprbars
@@ -84,25 +103,6 @@
 
       # What follows was copied straight from the default config
 
-      # https://wiki.hyprland.org/Configuring/Variables/#general
-      general {
-          gaps_in = 5
-          gaps_out = 20
-
-          border_size = 2
-
-          # https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
-          col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
-          col.inactive_border = rgba(595959aa)
-
-          # Set to true enable resizing windows by clicking and dragging on borders and gaps
-          resize_on_border = false
-
-          # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
-          allow_tearing = false
-
-          layout = dwindle
-      }
 
       # https://wiki.hyprland.org/Configuring/Variables/#decoration
       decoration {
