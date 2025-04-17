b53b469532d20c6d2d8dc8907204caeee53624bd
Chris West <cjwest4942@gmail.com>
2025-04-16 22:22:44 -0400

    reduce hyprlock grace period

--- home/desktop-environments/hyprland.nix
+++ home/desktop-environments/hyprland.nix
@@ -14,7 +14,7 @@
     settings = {
       general = {
         disable_loading_bar = true;
-        grace = 300;
+        grace = 4;
         hide_cursor = true;
         no_fade_in = false;
       };
