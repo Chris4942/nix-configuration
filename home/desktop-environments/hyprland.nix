cdd535abbefd1d23621637b8a01e321f790cc1c9
Chris West <cjwest4942@gmail.com>
2025-06-30 10:58:38 -0400

    fcitx5 is working now

--- home/desktop-environments/hyprland.nix
+++ home/desktop-environments/hyprland.nix
@@ -158,97 +158,100 @@
     plugins = with pkgs.hyprlandPlugins; [
       hyprbars
     ];
-    extraConfig = let
-      reset = "hyprctl dispatch submap reset;";
-    in ''
-      submap = launch
-              bind = , B, exec, ${reset} ${pkgs.brave}/bin/brave
-              bind = , S, exec, ${reset} ${pkgs.spotify}/bin/spotify
-              bind = , D, exec, ${reset} ${pkgs.discord}/bin/discord
-              bind = , U, exec, ${reset} ${pkgs.blueman}/bin/blueman-manager
-              bind = , P, exec, ${reset} ${pkgs.pavucontrol}/bin/pavucontrol
-              bind = , W, exec, ${reset} ${pkgs.wdisplays}/bin/wdisplays
-              bind = , catchall, submap, reset
-      submap = reset
+    extraConfig =
+      let
+        reset = "hyprctl dispatch submap reset;";
+      in
+      ''
+        submap = launch
+                bind = , B, exec, ${reset} ${pkgs.brave}/bin/brave
+                bind = , S, exec, ${reset} ${pkgs.spotify}/bin/spotify
+                bind = , D, exec, ${reset} ${pkgs.discord}/bin/discord
+                bind = , U, exec, ${reset} ${pkgs.blueman}/bin/blueman-manager
+                bind = , P, exec, ${reset} ${pkgs.pavucontrol}/bin/pavucontrol
+                bind = , W, exec, ${reset} ${pkgs.wdisplays}/bin/wdisplays
+                bind = , catchall, submap, reset
+        submap = reset
 
-      submap = lock
-              bind = , L, exec, ${reset} ${pkgs.hyprlock}/bin/hyprlock
-              bind = , X, exec, ${reset} ${pkgs.hypridle}/bin/hypridle
-      submap = reset
-
-
-      # What follows was copied straight from the default config
-
-
-      # https://wiki.hyprland.org/Configuring/Variables/#decoration
-      decoration {
-          rounding = 10
-          rounding_power = 2
-
-          # Change transparency of focused and unfocused windows
-          active_opacity = 1.0
-          inactive_opacity = 1.0
-
-          shadow {
-              enabled = true
-              range = 4
-              render_power = 3
-              color = rgba(1a1a1aee)
-          }
-
-          # https://wiki.hyprland.org/Configuring/Variables/#blur
-          blur {
-              enabled = true
-              size = 3
-              passes = 1
-
-              vibrancy = 0.1696
-          }
-      }
-
-      # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-      dwindle {
-          pseudotile = true # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-          preserve_split = true # You probably want this
-      }
-
-      # https://wiki.hyprland.org/Configuring/Variables/#input
-      input {
-          kb_layout = us
-          kb_variant =
-          kb_model =
-          kb_options =
-          kb_rules =
-
-          follow_mouse = 1
-
-          sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
-
-          touchpad {
-              natural_scroll = false
-          }
-      }
-
-      bindm = $mod, mouse:272, movewindow
-      bindm = $mod, mouse:273, resizewindow
-
-      # Laptop multimedia keys for volume and LCD brightness
-      bindel = ,XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
-      bindel = ,XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-      bindel = ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-      bindel = ,XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
-      bindel = ,XF86MonBrightnessUp, exec, brightnessctl s 10%+
-      bindel = ,XF86MonBrightnessDown, exec, brightnessctl s 10%-
+        submap = lock
+                bind = , L, exec, ${reset} ${pkgs.hyprlock}/bin/hyprlock
+                bind = , X, exec, ${reset} ${pkgs.hypridle}/bin/hypridle
+        submap = reset
 
 
-      # Requires playerctl
-      bindl = , XF86AudioNext, exec, ${pkgs.playerctl}/bin/playerctl next
-      bindl = , XF86AudioPause, exec, ${pkgs.playerctl}/bin/playerctl play-pause
-      bindl = , XF86AudioPlay, exec, ${pkgs.playerctl}/bin/playerctl play-pause
-      bindl = , XF86AudioPrev, exec, ${pkgs.playerctl}/bin/playerctl previous
-      exec-once = ${pkgs.waybar}/bin/waybar
-      exec-once = ${pkgs.hyprpaper}bin/hyprpaper
-      env = HYPRCURSOR_THEME,rose-pine-hyprcursor
-    '';
+        # What follows was copied straight from the default config
+
+
+        # https://wiki.hyprland.org/Configuring/Variables/#decoration
+        decoration {
+            rounding = 10
+            rounding_power = 2
+
+            # Change transparency of focused and unfocused windows
+            active_opacity = 1.0
+            inactive_opacity = 1.0
+
+            shadow {
+                enabled = true
+                range = 4
+                render_power = 3
+                color = rgba(1a1a1aee)
+            }
+
+            # https://wiki.hyprland.org/Configuring/Variables/#blur
+            blur {
+                enabled = true
+                size = 3
+                passes = 1
+
+                vibrancy = 0.1696
+            }
+        }
+
+        # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
+        dwindle {
+            pseudotile = true # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+            preserve_split = true # You probably want this
+        }
+
+        # https://wiki.hyprland.org/Configuring/Variables/#input
+        input {
+            kb_layout = us
+            kb_variant =
+            kb_model =
+            kb_options =
+            kb_rules =
+
+            follow_mouse = 1
+
+            sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
+
+            touchpad {
+                natural_scroll = false
+            }
+        }
+
+        bindm = $mod, mouse:272, movewindow
+        bindm = $mod, mouse:273, resizewindow
+
+        # Laptop multimedia keys for volume and LCD brightness
+        bindel = ,XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
+        bindel = ,XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+        bindel = ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+        bindel = ,XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
+        bindel = ,XF86MonBrightnessUp, exec, brightnessctl s 10%+
+        bindel = ,XF86MonBrightnessDown, exec, brightnessctl s 10%-
+
+
+        # Requires playerctl
+        bindl = , XF86AudioNext, exec, ${pkgs.playerctl}/bin/playerctl next
+        bindl = , XF86AudioPause, exec, ${pkgs.playerctl}/bin/playerctl play-pause
+        bindl = , XF86AudioPlay, exec, ${pkgs.playerctl}/bin/playerctl play-pause
+        bindl = , XF86AudioPrev, exec, ${pkgs.playerctl}/bin/playerctl previous
+        exec-once = ${pkgs.waybar}/bin/waybar
+        exec-once = ${pkgs.hyprpaper}bin/hyprpaper
+        env = HYPRCURSOR_THEME,rose-pine-hyprcursor
+        exec-once=fcitx5 -d # not ${pkgs.fcitx5}/bin/fcitx5 !
+      '';
   };
 }
