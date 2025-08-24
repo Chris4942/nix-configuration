110f398c05c9477ce94f02a2f013af1be37e297c
Chris West <cjwest4942@gmail.com>
2025-08-23 23:44:10 -0400

    enable fuzzy finder in telescope

--- modules/nvf/module.nix
+++ modules/nvf/module.nix
@@ -81,6 +81,17 @@
         tabline.nvimBufferline.enable = true;
         telescope = {
           enable = true;
+          extensions = [
+            {
+              name = "fzf";
+              packages = [ pkgs.vimPlugins.telescope-fzf-native-nvim ];
+              setup = {
+                fzf = {
+                  fuzzy = true;
+                };
+              };
+            }
+          ];
           setupOpts.defaults.vimgrep_arguments = [
             "${pkgs.ripgrep}/bin/rg"
             "--color=never"
