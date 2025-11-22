aa839d8d1d2681aa49a4442e2030587141187631
Chris West <cjwest4942@gmail.com>
2025-11-21 19:55:12 -0500

    first attempt on 31

--- hardware/machines/server/next-cloud.nix
+++ hardware/machines/server/next-cloud.nix
@@ -4,12 +4,18 @@
 }:
 {
   # This is the default password that will be overridden
+
+  environment.systemPackages = [ pkgs.nextcloud31 ];
   environment.etc."nextcloud-admin-pass".text = "yak_examples6CHEROKEE";
+  networking.firewall.allowedTCPPorts = [ 80 ];
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud32;
-    hostname = "localhost";
+    package = pkgs.nextcloud31;
+    hostName = "localhost";
     config.adminpassFile = "/etc/nextcloud-admin-pass";
     config.dbtype = "sqlite";
+    settings.trusted_domains = [
+      "192.168.1.207"
+    ];
   };
 }
