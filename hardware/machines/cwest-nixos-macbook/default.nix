6d231da0b550ffc0f50f2cc8bdfd98834f03cc37
Chris West <cjwest4942@gmail.com>
2025-12-06 07:57:08 -0500

    remove a bunch of extraneous configs

--- hardware/machines/cwest-nixos-macbook/default.nix
+++ hardware/machines/cwest-nixos-macbook/default.nix
@@ -13,18 +13,14 @@ nixpkgs.lib.nixosSystem {
     ./configuration.nix
     ../../../modules/fonts.nix
     ../../../modules/localization/new-york.nix
-    # ../../../modules/monitor-configuration.nix
     ../../../modules/bash.nix
-    ../../../modules/syncthing.nix
     ../../../modules/nix-ld.nix
     ../../../modules/xinput.nix
     ../../../modules/steam.nix
     ../../../modules/audio.nix
     ../../../modules/nvf/module.nix
     ../../../hardware/machines/cwest-nixos-macbook/hardware.nix
-    # ../../../hardware/drivers/amdgpu.nix
     ../../../modules/desktop-environment/gnome.nix
-    ../../../modules/desktop-environment/sway.nix
     ../../../modules/libre-office.nix
     ../../../modules/automated-garbage-collection.nix
     ../../../modules/desktop-environment/hyprland.nix
