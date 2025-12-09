bd11f4fb969385ce93d481a8cafe1b9c57d90dfc
Chris West <cjwest4942@gmail.com>
2025-12-08 21:19:21 -0500

    add zoxide to programs list and enable home-manager

--- hardware/machines/server/home/cwest.nix
+++ hardware/machines/server/home/cwest.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
   imports = [ ../../../../home/zoxide.nix ];
-
   home.stateVersion = "25.11";
+  programs.home-manager.enable = true;
 }
--- home/zoxide.nix
+++ home/zoxide.nix
@@ -1,5 +1,6 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  home.packages = with pkgs; [ zoxide ];
   programs.zoxide = {
     options = [ "--cmd cd" ];
     enable = true;
