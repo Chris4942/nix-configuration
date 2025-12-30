73e49b90d9500baacfa4607a58be0180cfccd8ae
Chris West <cjwest4942@gmail.com>
2025-12-30 00:59:19 -0500

    set up shortcuts for switching to the tv so that it's easy to get it out there

--- hardware/machines/cwest-nixos-1/users/cwest.nix
+++ hardware/machines/cwest-nixos-1/users/cwest.nix
@@ -1,4 +1,8 @@
 { ... }:
+let
+  # Require pressing all the buttons so I don't do this on accident
+  allModifiers = "$mod SHIFT CONTROL_L ALT_L";
+in
 {
   imports = [
     ../../../../users/cwest.nix
@@ -11,8 +15,22 @@
     };
   };
 
-  cwest.hyprland.extraBind = [ ];
-  cwest.hyprland.extraExtraConfig = ''
-    monitor = HDMI-A-1, disable
-  '';
+  cwest.hyprland.extraBind = [
+    "${allModifiers}, M, submap, monitors"
+  ];
+  cwest.hyprland.extraExtraConfig =
+    let
+      reset = "hyprctl dispatch submap reset;";
+    in
+    ''
+      monitor = HDMI-A-1, disable
+
+      submap = monitors
+        # To the TV; why Y? Because it mirrors T (for TV), but it can be pressed with my right hand
+        bind = ${allModifiers}, Y, exec, ${reset} hyprctl keyword monitor "HDMI-A-1, preferred, auto, 1" && hyprctl keyword monitor "DP-3, disabled"
+        # To the office
+        bind = ${allModifiers}, O, exec, ${reset} hyprctl keyword monitor "DP-3, preferred, auto, 1" && hyprctl keyword monitor "HDMI-A-1, disabled"
+        bind = , catchall, submap, reset
+      submap = reset
+    '';
 }
