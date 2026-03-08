343ca00bb9941eb117b8b741fcc16e3c63e2c16d
Chris West <cjwest4942@gmail.com>
2026-03-08 16:37:13 -0400

    format rust on save

--- modules/nvf/lua/conform.lua
+++ modules/nvf/lua/conform.lua
@@ -2,6 +2,7 @@ require("conform").setup({
         formatters_by_ft = {
                 lua = { "stylelua" },
                 nix = { lsp_format = "fallback" },
+                rust = { lsp_format = "fallback" },
         },
         format_on_save = { timeout_ms = 500 },
         default_format_opts = {},
