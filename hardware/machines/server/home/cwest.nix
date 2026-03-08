fbd80a9b21ae8e922d76d9cb8870849e2808327b
Chris West <cjwest4942@gmail.com>
2026-03-07 23:11:56 -0500

    include git.nix on server

--- hardware/machines/server/home/cwest.nix
+++ hardware/machines/server/home/cwest.nix
@@ -1,6 +1,9 @@
 { ... }:
 {
-  imports = [ ../../../../home/zoxide.nix ];
+  imports = [
+    ../../../../home/zoxide.nix
+    ../../../../home/git.nix
+  ];
   programs.bash.enable = true;
   home.stateVersion = "25.11";
   programs.home-manager.enable = true;
