f4090ed24e391732ef56a973c64bd0792b08c6f1
Chris West <cjwest4942@gmail.com>
2026-03-14 16:55:26 -0400

    fix the crontab so that it should actually run now

--- hardware/machines/server/cron.nix
+++ hardware/machines/server/cron.nix
@@ -3,7 +3,7 @@
   services.cron = {
     enable = true;
     systemCronJobs = [
-      "*/1 * * * *      root    ${pkgs.rsync}/bin/rsync -avn /mnt/main/ /mnt/backup/ >> /tmp/rsync.log"
+      "0 4 * * *      root    ${pkgs.rsync}/bin/rsync -avn /mnt/main/ /mnt/backup/ >> /tmp/rsync.log"
     ];
   };
 }
