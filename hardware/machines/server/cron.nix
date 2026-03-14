2d42f2f2142136d956e9d8d0506999e5a3a975a1
Chris West <cjwest4942@gmail.com>
2026-03-14 16:53:04 -0400

    fix typo

--- hardware/machines/server/cron.nix
+++ hardware/machines/server/cron.nix
@@ -3,7 +3,7 @@
   services.cron = {
     enable = true;
     systemCronJobs = [
-      "*/1 * * * *      root    ${pkgs.rysnc}/bin/rsync -avn /mnt/main/ /mnt/backup/ >> /tmp/rsync.log"
+      "*/1 * * * *      root    ${pkgs.rsync}/bin/rsync -avn /mnt/main/ /mnt/backup/ >> /tmp/rsync.log"
     ];
   };
 }
