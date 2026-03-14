a3e179f82330d1d3ec7e737b65db2cd0dc18b32a
Chris West <cjwest4942@gmail.com>
2026-03-14 15:55:05 -0400

    run a dry run every minute for testing

--- hardware/machines/server/cron.nix
+++ hardware/machines/server/cron.nix
@@ -3,7 +3,7 @@
   services.cron = {
     enable = true;
     systemCronJobs = [
-      "0 4 * * * *      root    rsync -av /mnt/main/ /mnt/backup/ >> /tmp/rsync.log"
+      "0 4 * * *      root    rsync -avn /mnt/main/ /mnt/backup/ >> /tmp/rsync.log"
     ];
   };
 }
