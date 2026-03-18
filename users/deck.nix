4bc5e1f4b85c67f399ee32675949abbf463d715c
Chris West <cjwest4942@gmail.com>
2026-03-17 21:34:17 -0400

    start getting zoxide working on the steam deck

--- users/deck.nix
+++ users/deck.nix
@@ -1,8 +1,12 @@
 { pkgs, ... }:
 {
-  imports = [ ../modules/nvf/config.nix ];
+  imports = [
+    ../modules/nvf/config.nix
+    ../home/zoxide.nix
+  ];
   home.username = "deck";
   home.homeDirectory = "/home/deck";
+  programs.bash.enable = true;
   home.stateVersion = "25.05";
   programs.home-manager.enable = true;
 }
