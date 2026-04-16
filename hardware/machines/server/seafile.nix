580b156a7a9e8f0d92ebfcb16a40ace17e3aa537
Chris West <cjwest4942@gmail.com>
2026-04-16 16:49:21 -0400

    start configuring seafile

---
+++ hardware/machines/server/seafile.nix
@@ -0,0 +1,13 @@
+{ ... }:
+{
+  services.seafile = { };
+  adminEmail = "cjwest4942@gmail.com";
+  initialAdminPassword = "initialAdminPassword";
+  ccnetSettings.General.SERVICE_URL = "https://seafile.example.com";
+  dataDir = "/mnt/main/seafile/";
+  seafileSettings = {
+    fileserver = {
+      host = "unix:/run/seafile/server.sock";
+    };
+  };
+}
