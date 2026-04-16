720342bd37c760d4b9efa3668a68c289cdcffde3
Chris West <cjwest4942@gmail.com>
2026-04-16 16:51:42 -0400

    fix nesting typo

--- hardware/machines/server/seafile.nix
+++ hardware/machines/server/seafile.nix
@@ -1,13 +1,14 @@
 { ... }:
 {
-  services.seafile = { };
-  adminEmail = "cjwest4942@gmail.com";
-  initialAdminPassword = "initialAdminPassword";
-  ccnetSettings.General.SERVICE_URL = "https://seafile.example.com";
-  dataDir = "/mnt/main/seafile/";
-  seafileSettings = {
-    fileserver = {
-      host = "unix:/run/seafile/server.sock";
+  services.seafile = {
+    adminEmail = "cjwest4942@gmail.com";
+    initialAdminPassword = "initialAdminPassword";
+    ccnetSettings.General.SERVICE_URL = "https://seafile.example.com";
+    dataDir = "/mnt/main/seafile/";
+    seafileSettings = {
+      fileserver = {
+        host = "unix:/run/seafile/server.sock";
+      };
     };
   };
 }
