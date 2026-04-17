1c46384363f0757ac11dddb6a72114a48bb05f16
Chris West <cjwest4942@gmail.com>
2026-04-17 01:13:41 -0400

    move docker compose files into nix configuration

--- hardware/machines/server/seafile/default.nix
+++ hardware/machines/server/seafile/default.nix
@@ -1,34 +1,4 @@
 { pkgs, ... }:
-let
-  seafileYml = pkgs.fetchurl {
-    url = "https://manual.seafile.com/13.0/repo/docker/ce/seafile-server.yml";
-    sha256 = "sha256-8lWsiMit+j20dcPGvH4XJHJbNkem53LGF/Y9raZGbQk=";
-  };
-
-  seadocYml = pkgs.fetchurl {
-    url = "https://manual.seafile.com/13.0/repo/docker/seadoc.yml";
-    sha256 = "sha256-NZpwzABtfoZ8jy7lt4U+OsMnf51mmplnIziWM4D0wNQ=";
-  };
-
-  caddyYml = pkgs.fetchurl {
-    url = "https://manual.seafile.com/13.0/repo/docker/caddy.yml";
-    sha256 = "sha256-pfJuflqyyYio3AVBEibjxHyNPKvzXQUD6HkaVc/EpTI=";
-  };
-
-  seafile-docker-compose-directory = pkgs.stdenv.mkDerivation {
-    pname = "seafile-docker-config";
-    version = "13.0";
-    dontUnpack = true;
-
-    installPhase = ''
-      mkdir -p $out
-      cp ${./.env} $out/.env
-      cp ${seafileYml} $out/seafile-server.yml
-      cp ${seadocYml} $out/seadoc.yml
-      cp ${caddyYml} $out/caddy.yml
-    '';
-  };
-in
 {
   systemd.services.seafile = {
     enable = true;
@@ -40,7 +10,7 @@ in
     serviceConfig = {
       Type = "oneshot";
       EnvironmentFile = "/mnt/main/seafile/secrets.env";
-      WorkingDirectory = "${seafile-docker-compose-directory}";
+      WorkingDirectory = "${./docker-compose}";
       ExecStart = "${pkgs.docker-compose}/bin/docker-compose up -d";
       ExecStop = "${pkgs.docker-compose}/bin/docker-compose down";
       RemainAfterExit = true;
