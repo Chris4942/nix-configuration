7f3b796810faff351aeae8212af0f782877f1c6b
Chris West <cjwest4942@gmail.com>
2026-05-21 00:04:02 -0400

    add rocm packages

--- hardware/machines/cwest-nixos-1/hardware.nix
+++ hardware/machines/cwest-nixos-1/hardware.nix
@@ -53,4 +53,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.graphics.extraPackages = with pkgs; [
+    rocmPackages.clr.icd
+  ];
 }
