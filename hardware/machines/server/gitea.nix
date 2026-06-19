4f80f187a2ead64d835557d8161aea213e833987
Chris West <cjwest4942@gmail.com>
2026-06-19 07:54:42 -0400

    add gitea nix

---
+++ hardware/machines/server/gitea.nix
@@ -0,0 +1,13 @@
+{ ... }:
+{
+  services.gitea = {
+    enable = true;
+    stateDir = "/mnt/main/gitea/";
+    settings = {
+      server = {
+        HTTP_ADDR = "west-server";
+        HTTP_PORT = "3000"; # The default, but explicit so I can tell what it is
+      };
+    };
+  };
+}
