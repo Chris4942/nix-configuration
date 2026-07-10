61d21ddcf6b97d3c8559b0614fa8e117e266b44d
Chris West <cjwest4942@gmail.com>
2026-07-10 16:11:18 -0400

    rename nixos-1 to walnut

--- hardware/machines/cwest-nixos-1/configuration.nix
+++ hardware/machines/cwest-nixos-1/configuration.nix
@@ -23,7 +23,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "walnut";
 
   # Enable networking
   networking.networkmanager.enable = true;
