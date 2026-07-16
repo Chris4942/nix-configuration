7aafb0005cd8c2cc1dadd2ca48b7d44f1e611075
Chris West <cjwest4942@gmail.com>
2026-07-16 01:34:53 -0400

    some random changes including being really aggressive with the palm even though it's not detecting anything

--- hardware/machines/virginia-greening/configuration.nix
+++ hardware/machines/virginia-greening/configuration.nix
@@ -75,7 +75,11 @@
     ++ (with pkgs; [
       seafile-client
       libinput
+      evtest
+      evemu
     ]);
+
+  virtualisation.docker.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
--- hardware/machines/virginia-greening/libinput.nix
+++ hardware/machines/virginia-greening/libinput.nix
@@ -11,10 +11,11 @@
   environment.etc."libinput/local-overrides.quirks".text = ''
     [Macbook Touchpad]
     MatchName=*Apple*Internal*Trackpad*
-    ModelAppleTouchpad=1
+    # ModelAppleTouchpad=1
     AttrSizeHint=162x100.5
     AttrTouchSizeRange=200:150
-    AttrPalmSizeThreshold=200
+    AttrPalmSizeThreshold=10
+    AttrPalmPressureThreshold=8
 
     [MacBook(Pro) SPI Keyboards]
     MatchName=*Apple SPI Keyboard*
