cb4a3d485d1214b9a58aa53f5e2ee9bb5db2c4f7
Chris West <cjwest4942@gmail.com>
2026-07-16 02:19:01 -0400

    add a comment explaining everything I did

--- hardware/machines/virginia-greening/libinput.nix
+++ hardware/machines/virginia-greening/libinput.nix
@@ -34,6 +34,15 @@
       {
         commands = [
           {
+            # This script is the solution to a very dumb problem.
+            # libinput does not correctly classify palm events even though the detection config above is correct.
+            # To find the trackpad, run `sudo libinput list-devices`.
+            # The keyboard and trackpad are together, so it will appear twice, but I care about the one that is a pointer.
+            # If I run sudo libinput record /dev/input/eventX > palm.yml, it will record the palm motion.
+            # If I replay it with sudo libinput replay palm.yml, then those events will be palm detect in the `sudo libinput debug-events` output.
+            # If I record events using `evemu-record` and then replay that to the _same_ device it came from palm detections are not detected;
+            # However, if I play those events back against a new device created from `sudo libinput replay`, then I _do_ get palm detections.
+            # Read and write to/from the events, by just redirecting stdin and stdout
             command = "${./toggle-mouse.sh}";
             options = [ "NOPASSWD" ];
           }
