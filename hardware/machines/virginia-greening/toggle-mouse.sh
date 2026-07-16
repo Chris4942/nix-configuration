7b0a604f884fbc306916f7a1d7b6716c3c40530e
Chris West <cjwest4942@gmail.com>
2026-07-16 18:31:15 -0400

    fix toggle script so that it works when the device id changes

--- hardware/machines/virginia-greening/toggle-mouse.sh
+++ hardware/machines/virginia-greening/toggle-mouse.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 set -eo pipefail
 
-DEVICE='0003:05AC:027C.0005'
+TRACKPAD_CACHE='/tmp/trackpad'
+
+if [ ! -f $TRACKPAD_CACHE ]; then
+        ls /sys/bus/hid/drivers/magicmouse/ | rg '0003:05AC:027C.000\d' | tr -d \\n > $TRACKPAD_CACHE
+fi
+
+DEVICE=`cat $TRACKPAD_CACHE`
+
 if (ls /sys/bus/hid/drivers/magicmouse/ | rg -q $DEVICE); then
         sudo echo $DEVICE > /sys/bus/hid/drivers/magicmouse/unbind
 else
