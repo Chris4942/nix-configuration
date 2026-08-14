c3cc7bae3c7671743075d4ff767dd45c8dca3eb4
Chris West <cjwest4942@gmail.com>
2026-08-14 13:53:20 -0400

    restart server every day at 3 instead of fixing the actual problem with tailscale

--- hardware/machines/server/cron.nix
+++ hardware/machines/server/cron.nix
@@ -4,6 +4,9 @@
     enable = true;
     systemCronJobs = [
       "0 4 * * *      root    ${pkgs.rsync}/bin/rsync -av /mnt/main/ /mnt/backup/ >> /tmp/rsync.log"
+      # Reboot everyday at 3 AM because sometimes tailscale gets out of wack and rebooting seems to fix it.
+      # This is a stopgap until I can fix that
+      "0 3 * * *      root    reboot"
     ];
   };
 }
--- hardware/machines/server/hardware.nix
+++ hardware/machines/server/hardware.nix
@@ -71,7 +71,6 @@
   # networking.interfaces.wlp15s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   hardware.graphics = {
     enable = true;
