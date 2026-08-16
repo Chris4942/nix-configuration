64b4cec5261f17568d81fcde5da85b0bb051e3aa
Chris West <cjwest4942@gmail.com>
2026-08-15 23:59:51 -0400

    add note about older generations ticking around without another command

--- modules/automated-garbage-collection.nix
+++ modules/automated-garbage-collection.nix
@@ -1,6 +1,9 @@
 {
   # this can be done manually instead with
   # nix-collect-garbage -d --delete-older-than 14d
+  #
+  # However, old generations will still stick around unless you run as well
+  # sudo nix-env --delete-generations 14d --profile /nix/var/nix/profiles/system
   nix.gc.automatic = true;
   nix.gc.dates = "monthly";
   nix.gc.options = "--delete-older-than 14d";
