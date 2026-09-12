7241f4642db95812c9660fc5bc0e17f287cf9abc
Chris West <cjwest4942@gmail.com>
2026-09-12 15:46:41 -0400

    move zwave secrets to a more sensible place

--- hardware/machines/server/zwave-js/module.nix
+++ hardware/machines/server/zwave-js/module.nix
@@ -5,6 +5,6 @@
     # Normally 3000, but gitea is on 3000, so zwave-js has to go on something else. 3500 is totally arbitrary.
     port = 3500;
     serialPort = "/dev/serial/by-id/usb-Zooz_800_Z-Wave_Stick_533D004242-if00";
-    secretsConfigFile = "/secrets/zwave-js-keys.json";
+    secretsConfigFile = "/etc/zwave-js/secrets.json";
   };
 }
