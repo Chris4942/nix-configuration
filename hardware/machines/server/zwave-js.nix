1c0741ab43ad7df7756f8e568e8b4c12dd627adf
Chris West <cjwest4942@gmail.com>
2026-09-12 15:12:15 -0400

    set up zwave-js

---
+++ hardware/machines/server/zwave-js.nix
@@ -0,0 +1,9 @@
+{
+
+  services.zwave-js = {
+    enable = true;
+    # Normally 3000, but gitea is on 3000, so zwave-js has to go on something else. 3500 is totally arbitrary.
+    port = 3500;
+    serialPort = "/dev/serial/by-id/usb-Zooz_800_Z-Wave_Stick_533D004242-if00";
+  };
+}
