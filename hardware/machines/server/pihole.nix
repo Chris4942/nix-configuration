0047ec7c9ba7b41dbb2f8dfb68b65c78cf7a71d0
Chris West <cjwest4942@gmail.com>
2026-09-24 18:41:37 -0400

    more pihole lists

--- hardware/machines/server/pihole.nix
+++ hardware/machines/server/pihole.nix
@@ -15,6 +15,18 @@
         enabled = true;
         description = "hagezi blocklist";
       }
+      {
+        url = "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/all_domains.txt";
+        type = "block";
+        enabled = true;
+        description = "Pi-hole Optimized Blocklists";
+      }
+      {
+        url = "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/nsfw.txt";
+        type = "block";
+        enabled = true;
+        description = "Pi-hole Optimized Blocklists NSFW";
+      }
     ];
   };
 
