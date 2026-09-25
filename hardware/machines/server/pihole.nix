4cc58e6c8402f033e5f71bf9def61b007e59edb3
Chris West <cjwest4942@gmail.com>
2026-09-24 20:49:31 -0400

    block list for smart tvs

--- hardware/machines/server/pihole.nix
+++ hardware/machines/server/pihole.nix
@@ -14,26 +14,32 @@
       };
     };
 
-    # lists = [
-    #   {
-    #     url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt";
-    #     type = "block";
-    #     enabled = true;
-    #     description = "hagezi blocklist";
-    #   }
-    #   {
-    #     url = "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/all_domains.txt";
-    #     type = "block";
-    #     enabled = true;
-    #     description = "Pi-hole Optimized Blocklists";
-    #   }
-    #   {
-    #     url = "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/nsfw.txt";
-    #     type = "block";
-    #     enabled = true;
-    #     description = "Pi-hole Optimized Blocklists NSFW";
-    #   }
-    # ];
+    lists = [
+      {
+        url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt";
+        type = "block";
+        enabled = true;
+        description = "hagezi blocklist";
+      }
+      {
+        url = "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/all_domains.txt";
+        type = "block";
+        enabled = true;
+        description = "Pi-hole Optimized Blocklists";
+      }
+      {
+        url = "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/nsfw.txt";
+        type = "block";
+        enabled = true;
+        description = "Pi-hole Optimized Blocklists NSFW";
+      }
+      {
+        url = "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/SmartTV.txt";
+        type = "block";
+        enabled = true;
+        description = "Smart TV Blocklist";
+      }
+    ];
 
     openFirewallDNS = true;
   };
