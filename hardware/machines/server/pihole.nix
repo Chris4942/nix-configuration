fae6925d610256c292baddd32b3551b0515afb3e
Chris West <cjwest4942@gmail.com>
2026-09-24 20:22:22 -0400

    remove the lists

--- hardware/machines/server/pihole.nix
+++ hardware/machines/server/pihole.nix
@@ -8,26 +8,26 @@
       ];
     };
 
-    lists = [
-      {
-        url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt";
-        type = "block";
-        enabled = true;
-        description = "hagezi blocklist";
-      }
-      {
-        url = "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/all_domains.txt";
-        type = "block";
-        enabled = true;
-        description = "Pi-hole Optimized Blocklists";
-      }
-      {
-        url = "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/nsfw.txt";
-        type = "block";
-        enabled = true;
-        description = "Pi-hole Optimized Blocklists NSFW";
-      }
-    ];
+    # lists = [
+    #   {
+    #     url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt";
+    #     type = "block";
+    #     enabled = true;
+    #     description = "hagezi blocklist";
+    #   }
+    #   {
+    #     url = "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/all_domains.txt";
+    #     type = "block";
+    #     enabled = true;
+    #     description = "Pi-hole Optimized Blocklists";
+    #   }
+    #   {
+    #     url = "https://media.githubusercontent.com/media/zachlagden/Pi-hole-Optimized-Blocklists/main/lists/nsfw.txt";
+    #     type = "block";
+    #     enabled = true;
+    #     description = "Pi-hole Optimized Blocklists NSFW";
+    #   }
+    # ];
 
     openFirewallDNS = true;
   };
